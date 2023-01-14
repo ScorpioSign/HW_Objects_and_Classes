e18f407c3c72d61acc32bb7fb2ec16ce543fa904
ы <nvsemyonova@mail.ru>
2023-01-14 20:43:40 +0300

    Выполнено домашнее задание по методам объектов

--- src/lessons/Author.java
+++ src/lessons/Author.java
@@ -1,18 +1,41 @@
 package lessons;
 
+import java.util.Objects;
+
 public class Author {
     private String authorName;
     private String authorSurname;
-    public Author(String authorName, String authorSurname)  {
+
+    public Author(String authorName, String authorSurname) {
         this.authorName = authorName;
         this.authorSurname = authorSurname;
 
     }
-        public String getAuthorName() {
+
+    public String getAuthorName() {
         return this.authorName;
     }
 
     public String getAuthorSurname() {
         return authorSurname;
+    }
+
+    @Override
+    public String toString() {
+        return this.authorName + this.authorSurname;
+    }
+    @Override
+    public boolean equals (Object other) {
+        if (this == other)
+            return true;
+        if (other == null || this.getClass() != other.getClass())
+            return false;
+        Author author = (Author) other;
+        return Objects.equals(authorName, author.authorName) && Objects.equals(authorSurname, author.authorSurname);
+
+    }
+    @Override
+    public int hashCode (){
+        return Objects.hash(authorName, authorSurname);
     }
 }
--- src/lessons/Main.java
+++ src/lessons/Main.java
@@ -7,20 +7,22 @@ public class Main {
     //Создайте сеттер для поля «Год публикации» у книги.
     //В методе main создайте несколько объектов «Книга» (достаточно двух) и несколько объектов «Автор» (достаточно тоже двух) и инициализируйте их. Учтите, что авторы являются обязательными и книги не могут создаваться без авторов.
     //Метод main не должен находиться в классах Book и Author.
-    //
     //Создайте отдельный класс для запуска приложения и объявите метод main в нем.
-    //
     //В том же методе main измените год публикации одной из книг с помощью сеттера.
     //Так как вы изучили геттеры и сеттеры, оставлять поля открытыми, без модификатора доступа private, недопустимо.
+    //Реализуйте методы toString, equals и hashCode в классах Author и Book, которые вы создали на прошлом уроке.
+    //Обратите внимание, что toString книги не должен дублировать код из toString автора, а должен делегировать (вызывать) его версию метода.
     public static void main(String[] args) {
         Author author1 = new Author("Margaret", "Mitchell");
         Author author2 = new Author("Stephen", " King");
+        System.out.println(author1);
+        System.out.println(author2);
         Book book1 = new Book("Gone With the Wind", author1, 1987);
         Book book2 = new Book("The Green Mile", author2, 2005);
         System.out.println(book1.getBookTitle() + " ; " + book1.getAuthor() + " ; " + book1.getPublishingYear());
-        System.out.println(book2.getBookTitle() + " ; " + book2.getAuthor() + " ; " + book2.getPublishingYear());
+        System.out.println(book2);
         book2.setYear(2008);
-        System.out.println(book2.getBookTitle() + " ; " + book2.getAuthor() + " ; " + book2.getPublishingYear());
+        System.out.println(book2);
 
 
 
